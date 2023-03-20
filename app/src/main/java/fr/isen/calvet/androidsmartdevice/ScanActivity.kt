815e8992c66983af43ec3dc486728b5361554f4a
Jules Calvet <jules.calvet@isen.yncrea.fr>
2023-03-20 11:24:14 +0100

    demander permission bluetooth connect

--- app/src/main/java/fr/isen/calvet/androidsmartdevice/ScanActivity.kt
+++ app/src/main/java/fr/isen/calvet/androidsmartdevice/ScanActivity.kt
@@ -1,19 +1,20 @@
 package fr.isen.calvet.androidsmartdevice
 
 import android.Manifest
+import android.Manifest.permission
+import android.app.AlertDialog
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import fr.isen.calvet.androidsmartdevice.databinding.ActivityScanBinding
+
 
 class ScanActivity : AppCompatActivity() {
     private lateinit var binding: ActivityScanBinding
@@ -28,32 +29,53 @@ class ScanActivity : AppCompatActivity() {
         binding = ActivityScanBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if (bluetoothAdapter == null) {
-            Toast.makeText(this, "Le Bluetooth n'est pas disponible sur ce téléphone", Toast.LENGTH_SHORT).show()
-        } else if (!bluetoothAdapter!!.isEnabled) {
-            Toast.makeText(this, "Le Bluetooth est désactivé", Toast.LENGTH_SHORT).show()
-        } else {
-            // Bluetooth est activé, continuer avec les interactions Bluetooth
-            binding.floatingActionButton.setOnClickListener {
-                if(!scanning) {
-                    binding.textView3.setText(R.string.scanning)
-                    binding.floatingActionButton.setImageResource(android.R.drawable.ic_media_pause)
-                    binding.progressBar.isIndeterminate = true
-                } else {
-                    binding.textView3.setText(R.string.toScan)
-                    binding.floatingActionButton.setImageResource(android.R.drawable.ic_media_play)
-                    binding.progressBar.isIndeterminate = false
+
+
+        if (ContextCompat.checkSelfPermission(this, permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED)
+        {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission.BLUETOOTH_CONNECT)) {
+                // Expliquer à l'utilisateur pourquoi la permission est nécessaire
+                AlertDialog.Builder(this)
+                    .setTitle("Autoriser la permission Bluetooth")
+                    .setMessage("L'application a besoin de la permission Bluetooth pour détecter les appareils à proximité.")
+                    .setPositiveButton("OK") { _, _ ->
+                        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 1)
+                    }
+                    .setNegativeButton("Annuler", null)
+                    .show()
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(permission.BLUETOOTH_CONNECT), 1)
+            }
+        }
+        else
+        {
+            if (bluetoothAdapter == null) {
+                Toast.makeText(this, "Le Bluetooth n'est pas disponible sur ce téléphone", Toast.LENGTH_SHORT).show()
+            } else if (!bluetoothAdapter!!.isEnabled) {
+                Toast.makeText(this, "Le Bluetooth est désactivé", Toast.LENGTH_SHORT).show()
+            } else {
+                // Bluetooth est activé, continuer avec les interactions Bluetooth
+                binding.floatingActionButton.setOnClickListener {
+                    if(!scanning) {
+                        binding.textView3.setText(R.string.scanning)
+                        binding.floatingActionButton.setImageResource(android.R.drawable.ic_media_pause)
+                        binding.progressBar.isIndeterminate = true
+                    } else {
+                        binding.textView3.setText(R.string.toScan)
+                        binding.floatingActionButton.setImageResource(android.R.drawable.ic_media_play)
+                        binding.progressBar.isIndeterminate = false
+                    }
+
+                    scanning = !scanning
+
+                    binding.recyclerView.layoutManager = LinearLayoutManager(this)
+                    val devices: ArrayList<String> = ArrayList<String>()
+                    devices.add("Jules")
+                    devices.add("Lab_IOT")
+                    devices.add("Lab")
+                    devices.add("Jules_IOT")
+                    binding.recyclerView.adapter = AdapterDevicesList(devices)
                 }
-
-                scanning = !scanning
-
-                binding.recyclerView.layoutManager = LinearLayoutManager(this)
-                val devices: ArrayList<String> = ArrayList<String>()
-                devices.add("Jules")
-                devices.add("Lab_IOT")
-                devices.add("Lab")
-                devices.add("Jules_IOT")
-                binding.recyclerView.adapter = AdapterDevicesList(devices = devices)
             }
         }
 
@@ -80,5 +102,24 @@ class ScanActivity : AppCompatActivity() {
             }
         }*/
 
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // La permission a été accordée
+            } else {
+                Toast.makeText(
+                    this,
+                    "Les permissions sont nécessaires pour utiliser la fonction de scan",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
     }
 }
